7c86f3dc700d50f34c5f1bf5d85c3c8dd4bfd70b
alisalali <alis.alali@hotmail.com>
2024-11-24 00:23:00 +0300

    10-sending-a-post-request

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -23,9 +23,16 @@ export default function Checkout() {
 
     const fd = new FormData(event.target);
     const customerData = Object.fromEntries(fd.entries());
-      console.log(customerData);
-      
-      
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: { items: cartCtx.items, customer: customerData },
+      }),
+    });
   }
   return (
     <Modal
@@ -36,9 +43,9 @@ export default function Checkout() {
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="Email" type="email" id="email" />
-        <Input label="Street Address" type="text" id="street-address" />
+        <Input label="Street Address" type="text" id="street" />
         <div className="control-row">
           <Input label="Postal Code" type="text" id="postal-code" />
           <Input label="City" type="text" id="city" />
